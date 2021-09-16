ca2521f36a4016ec16f5127d4c23536e4953f6e8
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-09-16 09:30:23 +0530

    tool bar and side nav started

--- fitness-tracker/src/app/app.component.ts
+++ fitness-tracker/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fitness-tracker';
+
+  @ViewChild('sideNav')
+  sideNav
+  openToggle()
+  {
+    console.log(this.sideNav);
+  }
 }
