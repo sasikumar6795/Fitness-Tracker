6effd5b1a135c947bbab4bce24b8263d7038e441
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-10-15 10:47:11 +0530

    data table sorting added

--- fitness-tracker/src/app/training/past-training/past-training.component.ts
+++ fitness-tracker/src/app/training/past-training/past-training.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
@@ -8,14 +9,20 @@ import { TrainingService } from '../training.service';
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.css']
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
 
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource=new MatTableDataSource<Exercise>();
+  @ViewChild(MatSort) sort: MatSort;
   constructor(private trainingService:TrainingService) { }
+  
 
   ngOnInit(): void {
     this.dataSource.data=this.trainingService.getCompletedOrCancelledExercise();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort=this.sort;
   }
 
 }
