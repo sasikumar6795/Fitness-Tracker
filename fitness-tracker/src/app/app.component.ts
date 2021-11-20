e950d4c8b20152e03e08db454f222ca10deaddf4
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-11-20 11:54:52 +0530

    optimised singled user login and logout method

--- fitness-tracker/src/app/app.component.ts
+++ fitness-tracker/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, Injectable, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+@Injectable()
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService){}
   title = 'fitness-tracker';
+
+  ngOnInit(): void {
+    this.authService.initAuthListener();
+    
+  }
 
  
 }
--- fitness-tracker/src/app/auth/auth.service.ts
+++ fitness-tracker/src/app/auth/auth.service.ts
@@ -12,13 +12,26 @@ export class AuthService{
     authChange = new Subject<boolean>();
 
     constructor(private router: Router, private authFir: AngularFireAuth, private trainingService:TrainingService){}
+    initAuthListener() {
+        this.authFir.authState.subscribe(user => {
+          if (user) {
+            this.isAuthenticated = true;
+            this.authChange.next(true);
+            this.router.navigate(['/training']);
+          } else {
+            this.trainingService.cancelSubscription();
+            this.authChange.next(false);
+            this.router.navigate(['/login']);
+            this.isAuthenticated = false;
+          }
+        });
+      }
     //when the user signup
     registerUser(authData: AuthData)
     {
        this.authFir.auth.createUserWithEmailAndPassword(authData.email,authData.password)
        .then(result => {
            console.log("signup",result);
-           this.authSuccessfully();
        })
        .catch(error => {
            console.log(error);
@@ -30,7 +43,6 @@ export class AuthService{
         this.authFir.auth.signInWithEmailAndPassword(authData.email,authData.password)
         .then(result => {
             console.log("login",result);
-            this.authSuccessfully();
         })
         .catch(error => {
             console.log(error);
@@ -39,11 +51,7 @@ export class AuthService{
 
     logOut()
     {
-        this.trainingService.cancelSubscription();
         this.authFir.auth.signOut();
-        this.authChange.next(false);
-        this.isAuthenticated=false;
-        this.router.navigate(['/login'])
     }
 
    
@@ -53,10 +61,5 @@ export class AuthService{
       return this.isAuthenticated;
     }
 
-    private authSuccessfully()
-    {
-        this.isAuthenticated=true;
-        this.authChange.next(false);
-        this.router.navigate(['/training'])
-    }
+   
 }
