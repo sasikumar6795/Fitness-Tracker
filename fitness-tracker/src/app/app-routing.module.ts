a6cf648691694f0880587c156e2ed4bfd4e265f7
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2022-01-04 07:52:06 +0530

    splitting routes

--- fitness-tracker/src/app/app-routing.module.ts
+++ fitness-tracker/src/app/app-routing.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthRouting } from './auth/auth-routing.module';
 import { AuthGuard } from './auth/auth.guard';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { TrainingComponent } from './training/training.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   {path : '', component: WelcomeComponent},
-  {path : 'signup', component: SignupComponent},
-  {path : 'login', component: LoginComponent},
   {path : 'training', component: TrainingComponent, canActivate: [AuthGuard]}
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),AuthRouting],
   exports: [RouterModule],
   providers:[AuthGuard]
 })
