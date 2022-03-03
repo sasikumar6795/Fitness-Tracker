dec4eaf3ceb6ded85377f2176037b43c8efb1e2a
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2022-03-03 16:58:42 +0530

    prod config updated

--- fitness-tracker/src/environments/environment.prod.ts
+++ fitness-tracker/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAASegTjNbNURqDkHR22xFt33bh1xAg6xw",
+    authDomain: "fitness-tracker-17f05.firebaseapp.com",
+    projectId: "fitness-tracker-17f05",
+    storageBucket: "fitness-tracker-17f05.appspot.com",
+    messagingSenderId: "288938560210",
+    appId: "1:288938560210:web:afe61b9322259c8fd4fb0b"
+  }
 };
